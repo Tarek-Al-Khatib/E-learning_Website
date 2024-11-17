36ae0d5d22981a9b2cc574a4e97531e497efd3bd
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-17 16:17:06 +0200

    Added main components to the admin dashboard page

--- frontend/elearning/src/pages/AdminDashboard.js
+++ frontend/elearning/src/pages/AdminDashboard.js
@@ -2,8 +2,31 @@ import React from "react";
 
 const AdminDashboard = () => {
   return (
-    <div>
-      <div></div>
+    <div className="dashboard-container">
+      <div className="sidebar">
+        <h2>Admin Dashboard</h2>
+        <ul className="sidebar-menu">
+          <li>
+            <button className="sidebar-button">Students</button>
+          </li>
+          <li>
+            <button className="sidebar-button">Instructors</button>
+          </li>
+          <li>
+            <button className="sidebar-button">Courses</button>
+          </li>
+          <li>
+            <button className="sidebar-button">Instructor</button>
+          </li>
+          <li>
+            <button className="sidebar-button">Course</button>
+          </li>
+        </ul>
+      </div>
+
+      <div className="main-content">
+        <h2>Welcome to the Admin Dashboard</h2>
+      </div>
     </div>
   );
 };
