6e4245bf60d2c66a4e6d50e26c820e85eec89d41
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-18 18:19:10 +0200

    Created student dashboard

---
+++ frontend/elearning/src/pages/StudentDashboard.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const StudentDashboard = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default StudentDashboard;
