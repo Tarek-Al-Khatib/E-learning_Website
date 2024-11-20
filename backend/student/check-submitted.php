6f20494f20246982c008c54ade1b57e2c4dd8e03
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-11-21 00:38:48 +0200

    Created Check submission api

---
+++ backend/student/check-submitted.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../connection.php"; 
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header('Content-Type: application/json');
+
+$student_id = $_GET['student_id'];
+$assignment_id = $_GET['assignment_id'];
+
+if($student_id && $assignment_id){
+  $query = $connection->prepare("SELECT * FROM submissions WHERE assignment_id = ? AND student_id = ?");
+  $query->bind_param("ii", $assignment_id, $student_id);
+  $query->execute();
+  $result = $query->get_result();
+  if($result->num_rows > 0){
+    echo json_encode(["value"=> true]);
+  }else {
+    echo json_encode(["value" => false]);
+  }
+}else {
+  echo json_encode(["message" => "Student id and assignment id are required"]);
+}
